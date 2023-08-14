37dd91e224edd6cbbdb6161c21ab9e6a9fa18ca6
UIT-22730080 <140036411+UIT-22730080@users.noreply.github.com>
2023-08-14 10:55:02 +0700

    Update GamePanel.java
    Chỉnh sửa ngày 14/8

--- SnakeGame-main/SnakeGame-main/src/GamePanel.java
+++ SnakeGame-main/SnakeGame-main/src/GamePanel.java
@@ -43,35 +43,6 @@ public class GamePanel extends JPanel implements ActionListener {
 		draw(g);
 	}
 
-	public void draw(Graphics g) {
-
-		if (running) {
-			// for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
-			// g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
-			// g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
-			// }
-			g.setColor(Color.red);
-			g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
-
-			for (int i = 0; i < bodyParts; i++) {
-				if (i == 0) {
-					g.setColor(Color.green);
-					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-				} else {
-					g.setColor(new Color(45, 180, 0));
-					// g.setColor((Color.pink));
-					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-				}
-			}
-			g.setColor(Color.RED);
-			g.setFont(new Font("Arial", Font.BOLD, 40));
-			FontMetrics metrics = getFontMetrics(g.getFont());
-			g.drawString("Score: " + appleEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: " + appleEaten)) / 2,
-					g.getFont().getSize());
-		} else {
-			gameOver(g);
-		}
-	}
 
 	public void newApple() {
 		appleX = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
